ef524b929f3c3e2e546c6dc4073e2831b6e278cd
Honchar007 <andrey2002335@gmail.com>
2019-11-23 14:40:56 +0200

    Add private

--- PracticeWithClass.cpp
+++ PracticeWithClass.cpp
@@ -4,20 +4,24 @@
 using namespace std;
  class Numbers 
 {
-public : 
-	int n1, n2;
+	 
+private : 
+	int n1, n2,n3;
     string name;
-	void set(int n1,int n2,string n = "NONAME")
+public:
+	void set(int n1,int n2,int n3,string n = "NONAME")
 	{
 		this->name = n;
 		this->n1 = n1;
 		this->n2 = n2;
+		this->n3 = n3;
 	}
 	void print()
 	{
-		cout << "The arguments of"<< name << endl;
+		cout << "The arguments of "<< name << endl;
 		cout << "The first number: " << n1 << endl;
 		cout << "The second number: " << n2 << endl;
+		cout << "The third number: " << n3 << endl;
 	}
  };
 
@@ -25,9 +29,9 @@ int main()
 {
     std::cout << "Hello World!\n"; 
 	Numbers n;
-	n.set(2, 4,"Hello");
-	Numbers n2{ 4,4 ,"Hello2" };
+	n.set(2, 4,4);
+	
 	n.print();
-	n2.print();
+	
 }
 
